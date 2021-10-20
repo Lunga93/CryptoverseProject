5bedd5b6e225aed16100d88f2fbe8986dc190159
Saint2209 <47632741+Saint2209@users.noreply.github.com>
2021-10-20 20:37:50 +0200

    coin test

---
+++ cryptoverse/coin.php
@@ -0,0 +1,43 @@
+<html>
+    <body>
+    <?php
+
+        //API KEYS:  C938843F-78B2-4848-973A-77385C2A2E99 ; 71E2C732-2B51-47BA-87EC-4FE08B2A4796
+         //connect to database
+        require_once("../config.php");
+        //array of coins
+        $coins_USD = array('http://rest.coinapi.io/v1/exchangerate/BTC/USD',
+                           'http://rest.coinapi.io/v1/exchangerate/ETH/USD',
+                           'http://rest.coinapi.io/v1/exchangerate/BNB/USD',
+                           'http://rest.coinapi.io/v1/exchangerate/ADA/USD',
+                           'http://rest.coinapi.io/v1/exchangerate/USDT/USD',
+                           'http://rest.coinapi.io/v1/exchangerate/XRP/USD',
+                           'http://rest.coinapi.io/v1/exchangerate/SOL/USD',
+                           'http://rest.coinapi.io/v1/exchangerate/DOT/USD',
+                           'http://rest.coinapi.io/v1/exchangerate/USDC/USD',
+                           'http://rest.coinapi.io/v1/exchangerate/DOGE/USD');
+
+        // make connection to DB
+        $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE)
+        or die("<p style=\"color: red;\">Could not connect to database!</p>");
+
+        
+        
+        for($count = 0; $count<10; $count++ ){
+            $ch = curl_init();
+            curl_setopt_array($ch, array(
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => "https://pure-woodland-29790.herokuapp.com/coins"
+            ));
+            $response = json_decode(curl_exec($ch),true);
+            var_dump($response);
+            // echo "\n\n";
+            // // echo $response -> rate ;
+            // echo "\n\n";
+            // print_r($response['rate']) ;
+
+
+?>
+    </body>
+</html>
+
